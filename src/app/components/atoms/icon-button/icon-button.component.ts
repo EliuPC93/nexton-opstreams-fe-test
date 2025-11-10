a400b1ba90ee0f41104627ae1048eb7f3ac4ce65
Eliu Palma <irvingeliu.93@gmail.com>
2025-11-10 12:14:44 -0600

    refactor: dynamically handle button values

--- src/app/components/atoms/icon-button/icon-button.component.ts
+++ src/app/components/atoms/icon-button/icon-button.component.ts
@@ -2,18 +2,24 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { NgClass } from "@angular/common";
 
 @Component({
-    selector: 'app-icon-button',
-    imports: [NgClass],
-    templateUrl: './icon-button.component.html',
-    styleUrl: './icon-button.component.scss'
+	selector: 'app-icon-button',
+	imports: [NgClass],
+	templateUrl: './icon-button.component.html',
+	styleUrl: './icon-button.component.scss'
 })
 export class IconButtonComponent {
-    @Input() iconName: string = '';
-    @Input() label: string = '';
+	@Input() key: string = '';
+	@Output() select = new EventEmitter<string>();
 
-    @Output() select = new EventEmitter<string>();
+	onSelect() {
+		this.select.emit(this.key);
+	}
 
-    onSelect() {
-        this.select.emit(this.iconName);
-    }
+	getClass(value: string): string {
+		return value.toLocaleLowerCase().slice(0, value.indexOf('-'));
+	}
+
+	getLabel(value: string): string {
+		return value.charAt(0).toUpperCase() + this.getClass(value).slice(1);
+	}
 }
--- src/app/select/select.component.ts
+++ src/app/select/select.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IconButtonComponent } from "../components/atoms/icon-button/icon-button.component";
 import { NgClass } from '@angular/common';
+import { ProcurementService } from '../procurement.service';
+import { ProductRequest } from '../product-requests';
 
 @Component({
-  selector: 'app-select',
-  imports: [IconButtonComponent, NgClass],
-  templateUrl: './select.component.html',
-  styleUrl: './select.component.scss'
+	selector: 'app-select',
+	providers: [ProcurementService],
+	imports: [IconButtonComponent, NgClass],
+	templateUrl: './select.component.html',
+	styleUrl: './select.component.scss'
 })
-export class SelectComponent {
+export class SelectComponent implements OnInit {
+	selected: string = '';
+	productSchemas: ProductRequest[] = [];
 
-  selected: string = '';
+	constructor(private procurementService: ProcurementService) { }
 
-  setSelection(selection: string) {
-    this.selected = selection;
-  }
+	ngOnInit() {
+		this.procurementService.getSchemas().subscribe((schemas: ProductRequest[]) => this.productSchemas = schemas);
+	}
+
+	setSelection(selection: string) {
+		this.selected = selection;
+	}
 }
