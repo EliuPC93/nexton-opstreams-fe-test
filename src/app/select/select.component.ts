b297ba63c910bbafd3a65e72afd71ee50c594299
Eliu Palma <irvingeliu.93@gmail.com>
2025-11-10 10:28:41 -0600

    refactor: change button color on selection

--- src/app/select/select.component.ts
+++ src/app/select/select.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { IconButtonComponent } from "../components/atoms/icon-button/icon-button.component";
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-select',
-  imports: [IconButtonComponent],
+  imports: [IconButtonComponent, NgClass],
   templateUrl: './select.component.html',
   styleUrl: './select.component.scss'
 })
 export class SelectComponent {
 
+  selected: string = '';
+
+  setSelection(selection: string) {
+    this.selected = selection;
+  }
 }
