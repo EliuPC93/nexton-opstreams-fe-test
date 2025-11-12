ab8601522208562aa94aef437831a69723ef96a1
Eliu Palma <irvingeliu.93@gmail.com>
2025-11-12 16:10:57 -0600

    refactor: small modification to currentSection object

--- src/app/questions/section/section.component.ts
+++ src/app/questions/section/section.component.ts
@@ -1,8 +1,8 @@
 import { AfterViewChecked, Component, OnInit, ChangeDetectorRef } from '@angular/core';
-import { CurrentSchema, SchemaService } from '../../section.service';
+import { SchemaService } from '../../section.service';
 import { ActionButtonComponent } from '../../components/atoms/action-button/action-button.component';
 import { Router } from '@angular/router';
-import { Section } from '../../product-requests';
+import { ProductRequest, Section } from '../../product-requests';
 import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
@@ -12,35 +12,35 @@ import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
 	imports: [ActionButtonComponent, ReactiveFormsModule]
 })
 export class SectionComponent implements OnInit, AfterViewChecked {
-	currentSchema: CurrentSchema | undefined;
 	currentSection : Section = { id: '', title: '', fields: [] };
 	sectionIndex: number = 0;
 	isLastIndex: boolean = false;
+	currentSchema: ProductRequest | undefined;
 	questionsForm: FormGroup | undefined;
 	currentFormGroup: FormGroup | undefined;
 
 	constructor(private router: Router, private sectionService: SchemaService, private changeDetector: ChangeDetectorRef) { }
 
 	ngOnInit(): void {
-		this.sectionService.getSchema$().subscribe(schema => {
+		this.sectionService.getSchema$().subscribe(({schema, index}) => {
 			this.currentSchema = schema;
-			this.sectionIndex = schema.index;
-			this.isLastIndex = this.sectionIndex === schema.schema.sections.length - 1;
-			this.currentSection = this.currentSchema.schema.sections[this.sectionIndex];
+			this.sectionIndex = index;
+			this.isLastIndex = this.sectionIndex === this.currentSchema.sections.length - 1;
+			this.currentSection = this.currentSchema.sections[this.sectionIndex];
 			this.currentFormGroup = undefined;
 		});
 	}
 
 	ngAfterViewChecked(): void {
 		if (!this.currentSchema) return;
-		this.currentFormGroup = this.buildFormControls(this.currentSchema.schema.sections).get(this.currentSchema.schema.sections[this.sectionIndex].id) as FormGroup;
+		this.currentFormGroup = this.buildFormControls(this.currentSchema.sections).get(this.currentSchema.sections[this.sectionIndex].id) as FormGroup;
 		this.changeDetector.detectChanges();
 	}
 
 	goToPage(pageToGo = this.sectionIndex) {
 		if (!this.currentSchema) return;
 		this.router.navigate([pageToGo + 1], { relativeTo: this.router.routerState.root.firstChild });
-		this.sectionService.setSchema(this.currentSchema.schema, pageToGo);
+		this.sectionService.setSchema(this.currentSchema, pageToGo);
 	}
 
 	buildFormControls(sections: Section[]): FormGroup {
