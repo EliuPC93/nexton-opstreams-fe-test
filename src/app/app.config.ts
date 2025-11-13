52c3f40f43ed7e08ed74849e553fc69e7fd8b3aa
Eliu Palma <irvingeliu.93@gmail.com>
2025-11-13 17:20:06 -0600

    setup in-memory-db and return schemas

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,23 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { RouteGuardService } from './route-guard.service';
+import { provideHttpClient } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { DataService } from './data.service';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideZoneChangeDetection({ eventCoalescing: true }),
 		provideRouter(routes, withViewTransitions()),
-		RouteGuardService
+		RouteGuardService,
+		provideHttpClient(),
+		importProvidersFrom(
+			HttpClientInMemoryWebApiModule.forRoot(DataService, {
+				delay: 500,
+				passThruUnknownUrl: true
+			})
+		)
 	]
 };
